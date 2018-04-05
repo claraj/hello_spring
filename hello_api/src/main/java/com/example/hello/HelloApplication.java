078c4e2673d58610dadeda1685310812246d5e25
clara <clara@minneapolis.edu>
2018-04-05 14:03:23 -0500

    hello world compliment API

---
+++ hello_api/src/main/java/com/example/hello/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@SpringBootApplication
+@Controller
+public class HelloApplication {
+
+
+	@RequestMapping("/")
+    @ResponseBody
+	String home() {
+		return "Please visit /random for a random compliment.";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+}
